265a3d8db34186da1305e6f7a300131eba8c3950
Bryan Saunders <bsaunder@redhat.com>
2020-05-05 22:06:45 -0400

    Working out the Kinks in the Dev Process

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-/**
- * Creates a Google Doc and sends an email to the current user with a link to the doc.
- */
-function createAndSendDocument() {
-  const email = Session.getActiveUser().getEmail();
-
-  // Get the name of the document to use as an email subject line.
-  const subject = 'This is a GAS Test';
-
-  // Append a new string to the "url" variable to use as an email body.
-  const body = `Test Test Test`;
-
-  // Send yourself an email with a link to the document.
-  GmailApp.sendEmail(email, subject, body);
+function testAll(){
+ 
+  var tc = new TrelloConfig('123')
+  Logger.log(tc.boardId)
+  
 }
-
-global.createAndSendDocument = createAndSendDocument;
---
+++ src/trello/TrelloConfig.js
@@ -0,0 +1,5 @@
+class TrelloConfig {
+  constructor(boardId){
+    this.boardId = boardId
+  }
+}
